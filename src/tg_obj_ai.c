5ba0545fbe235705c2cddaa6477dd3657e1c56b3
John Clements <clements.jm@gmail.com>
2026-03-22 22:38:41 -0400

    Updated blastic action reward

--- src/tg_obj_ai.c
+++ src/tg_obj_ai.c
@@ -219,7 +219,7 @@ void get_reward(tg_state* state_ctx, tank_ctx* tank, float* state, float* reward
     //reward[2] = ((float)tank->hits - 1.1*(float)tank->damage) / 100.0f;
 
     if (tank->fired  > 0)
-        reward[2] = (float)tank->hits - ((float)tank->fired - (float)tank->hits);
+        reward[2] = ((float)tank->hits - ((float)tank->fired - (float)tank->hits)) / (float)tank->fired;
 #endif
 }
 
